fa7bbb276dc11ce2554a1dbb41dd8a69d7503393
Facundo Casco <fcasco@gmail.com>
2016-04-22 20:54:21 -0300

    using class names instead of ids in the bits

--- bintim/bintim.js
+++ bintim/bintim.js
@@ -7,7 +7,65 @@
     "use strict";
     window.BinTim = BinTim;
 
+    var clock_container = document.getElementById('bintim_clock');
+
     BinTim.version = '0.1.0';
+
+    BinTim.stop = function stop() {
+        window.clearInterval(BinTim.inverval_id);
+    };
+
+    function update_bits(bits_container, bits) {
+        var bits_count = bits_container.getElementsByClassName('bit').length,
+            bit, i;
+
+        for (i = 0; i <= bits_count; i++) {
+            bit = bits_container.getElementsByClassName('bit_' + i);
+
+            if (bit.length > 0) {
+                bit = bit[0];
+            } else {
+                console.log('missing bit ' + i + ' element');
+                return;
+            }
+
+            if (bits[i] === '1') {
+                bit.className = bit.className.replace(/\boff\b/, 'on');
+            } else {
+                bit.className = bit.className.replace(/\bon\b/, 'off');
+            }
+        }
+    }
+
+    function update_hours(hour_bits) {
+        var hours_container = clock_container.getElementsByClassName('hours');
+
+        if (hours_container.length > 0) {
+            update_bits(hours_container[0], hour_bits);
+        } else {
+            console.log('missing hours element');
+        }
+    }
+
+    function update_minutes(minute_bits) {
+        var minutes_container = clock_container.getElementsByClassName('minutes');
+
+        if (minutes_container.length > 0) {
+            update_bits(minutes_container[0], minute_bits);
+        } else {
+            console.log('missing minutes element');
+        }
+    }
+
+    function update_seconds(second_bits) {
+        var seconds_container = clock_container.getElementsByClassName('seconds');
+
+        if (seconds_container.length > 0) {
+            update_bits(seconds_container[0], second_bits);
+        } else {
+            console.log('missing seconds element');
+        }
+    }
 
     function update_clock() {
         var now = new Date(),
@@ -16,34 +74,29 @@
             second = now.getSeconds().toString(2),
             hour_bits = ('00000' + hour).substring(hour.length),
             minute_bits = ('000000' + minute).substring(minute.length),
-            second_bits = ('000000' + second).substring(second.length),
-            i;
+            second_bits = ('000000' + second).substring(second.length);
 
-        for (i = 0; i <= 4; i++) {
-            if (hour_bits[i] === '1') {
-                document.getElementById('hours_b' + i).className = 'on';
-            } else {
-                document.getElementById('hours_b' + i).className = 'off';
-            }
-        }
-
-        for (i = 0; i <= 5; i++) {
-            if (minute_bits[i] === '1') {
-                document.getElementById('minutes_b' + i).className = 'on';
-            } else {
-                document.getElementById('minutes_b' + i).className = 'off';
-            }
-        }
-
-        for (i = 0; i <= 5; i++) {
-            if (second_bits[i] === '1') {
-                document.getElementById('seconds_b' + i).className = 'on';
-            } else {
-                document.getElementById('seconds_b' + i).className = 'off';
-            }
-        }
+        update_hours(hour_bits);
+        update_minutes(minute_bits);
+        update_seconds(second_bits);
     }
 
-    BinTim.inverval_id = window.setInterval(update_clock, 100);
+    BinTim.start = function start() {
+        BinTim.inverval_id = window.setInterval(update_clock, 100);
+    };
+
+    Array.prototype.map.call(clock_container.getElementsByClassName('start'),
+        function (elem) {
+            elem.addEventListener('click', BinTim.start);
+        }
+    );
+
+    Array.prototype.map.call(clock_container.getElementsByClassName('stop'),
+        function (elem) {
+            elem.addEventListener('click', BinTim.stop);
+        }
+    );
+
+    // BinTim.start();
 
 }(window.BinTim || {}));
